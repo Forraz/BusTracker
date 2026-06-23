72b9a2e0c56f186c24fabe260074655a8eb3c077
Forraz <forraz222@gmail.com>
2026-06-23 13:57:10 +0200

    Rewrite csv data import to retain relations on import

--- Backend/src/services/gtfs.import.service.ts
+++ Backend/src/services/gtfs.import.service.ts
@@ -1,35 +1,34 @@
 import { from } from "pg-copy-streams"
 import { createReadStream } from "node:fs";
 import { pipeline } from "node:stream/promises";
-import type { PgTable } from "drizzle-orm/pg-core";
+import { PgTable } from "drizzle-orm/pg-core";
+import type { PoolClient } from "pg";
 
 import { Service } from "../core/service.js"; 
-import { pool } from "../db/client.js";
 import { getTableName } from "drizzle-orm";
+
+import { pool } from "../db/client.js";
+import { routesTable, shapesTable, stopsTable, stopTimesTable, tripsTable } from "../db/schema.js";
 
 export class GTFSImportService extends Service {
 
-	public async loadDataToTable(csvPath: string, table: PgTable) {
+	public async loadData() {
 
-		const client = await pool.connect();
+		const client: PoolClient =  await pool.connect();
+		const tables: Array<PgTable> = [routesTable, shapesTable, tripsTable, stopsTable, stopTimesTable];
 
 		try {
 
-			const tableName = getTableName(table);
-			const newTableName = `${tableName}_new`;  
-			
 			await client.query("BEGIN;");
 
-			await client.query(`CREATE TABLE ${newTableName} AS SELECT * FROM ${tableName} WHERE false;`);
+			await client.query(`TRUNCATE ${tables.map(t => getTableName(t)).join(', ')} RESTART IDENTITY CASCADE;`)
 
-			const ingestStream = client.query(from(`COPY ${newTableName} FROM STDIN DELIMITER ',' CSV HEADER;`));
-			const sourceStream = createReadStream(csvPath);
-			await pipeline(sourceStream, ingestStream);
+			for (const table of tables) {
 
-			await client.query(`ALTER TABLE ${tableName} RENAME TO ${tableName}_tmp;`);
-			await client.query(`ALTER TABLE ${newTableName} RENAME TO ${tableName};`);
+				const csvPath = `./data/static-data/${getTableName(table)}.txt`;
+				await this.loadDataToTable(csvPath, table, client); 
 
-			await client.query(`DROP TABLE ${tableName}_tmp;`);
+			}
 
 			await client.query("COMMIT;");
 
@@ -44,6 +43,55 @@ export class GTFSImportService extends Service {
 
 		}
 
+
 	}
+
+	private async loadDataToTable(csvPath: string, table: PgTable, client: PoolClient) {
+
+		const tableName = getTableName(table);
+
+		const ingestStream = client.query(from(`COPY ${tableName} FROM STDIN DELIMITER ',' CSV HEADER;`));
+		const sourceStream = createReadStream(csvPath);
+
+		await pipeline(sourceStream, ingestStream);
+
+	}
+
+	// private async loadDataToTable(csvPath: string, table: PgTable) {
+	//
+	// 	const client = await pool.connect();
+	//
+	// 	try {
+	//
+	// 		const tableName = getTableName(table);
+	// 		const newTableName = `${tableName}_new`;  
+	//
+	// 		await client.query("BEGIN;");
+	//
+	// 		await client.query(`CREATE TABLE ${newTableName} (LIKE ${tableName} INCLUDING ALL)`);
+	//
+	// 		const ingestStream = client.query(from(`COPY ${newTableName} FROM STDIN DELIMITER ',' CSV HEADER;`));
+	// 		const sourceStream = createReadStream(csvPath);
+	// 		await pipeline(sourceStream, ingestStream);
+	//
+	// 		await client.query(`ALTER TABLE ${tableName} RENAME TO ${tableName}_tmp;`);
+	// 		await client.query(`ALTER TABLE ${newTableName} RENAME TO ${tableName};`);
+	//
+	// 		await client.query(`DROP TABLE ${tableName}_tmp;`);
+	//
+	// 		await client.query("COMMIT;");
+	//
+	// 	} catch (e) {
+	//
+	// 		await client.query("ROLLBACK;")
+	// 		console.log(e);
+	//
+	// 	} finally {
+	//
+	// 		client.release();
+	//
+	// 	}
+	//
+	// }
 
 }
