dbf4092063349dce950729afce881dde9335623e
Forraz <forraz222@gmail.com>
2026-07-05 18:28:43 +0200

    Export types from gtfs feed

---
+++ Backend/src/types/gtfs.ts
@@ -0,0 +1,16 @@
+import { transit_realtime } from "../gtfs/gtfs-rt.js";
+
+export type FeedEntity = transit_realtime.FeedEntity;
+export type FeedMessage = transit_realtime.FeedMessage;
+export type FeedHeader = transit_realtime.FeedHeader;
+
+export type VehiclePosition = transit_realtime.VehiclePosition;
+export type TripDescriptor = transit_realtime.TripDescriptor;
+export type Position = transit_realtime.Position;
+export type VehicleDescriptor = transit_realtime.VehicleDescriptor;
+
+export type TripUpdate = transit_realtime.TripUpdate;
+export type Alert = transit_realtime.Alert
+
+export type TranslatedString = transit_realtime.TranslatedString;
+export type TimeRange = transit_realtime.TimeRange;
