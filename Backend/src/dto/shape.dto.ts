d352e0b2018af7d164b43778471c536aa5493dfd
Forraz <forraz222@gmail.com>
2026-07-17 16:17:34 +0200

    Implement shape dto

---
+++ Backend/src/dto/shape.dto.ts
@@ -0,0 +1,41 @@
+import type { Shape } from "../db/schema.js"
+import type { Coordinates } from "../types/coordinates.js"
+
+export interface ShapeDTO {
+
+	id: string,
+	parts: {
+		coordinates: Coordinates,
+		distTraveled: number
+	}[]
+
+}
+
+export function mapShapeToDTO(shape: Shape[]): ShapeDTO {
+
+	const shapeId = shape[0]?.id!;
+
+	const shapeParts = shape.map((s) => {
+
+		const part = {
+
+			coordinates: {
+				lat: s.shapePtLat!,
+				lon: s.shapePtlon!
+			},
+			distTraveled: s.shapeDistTraveled!
+
+		}
+
+		return part;
+
+	});
+
+	const dto = {
+		id: shapeId,
+		parts: shapeParts
+	}
+
+	return dto;
+
+}
